07e9e979a38458246ece71a8848ddb2078e5123a
FidaAlSughayer <f.a.sghyr@gmail.com>
2016-04-17 23:46:04 -0700

    initial commit. status vis

---
+++ interactive-viewer/gantt.js
@@ -0,0 +1,92 @@
+var tasks = [
+{"startDate":new Date("Sun Feb 19 10:02:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:29:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:28:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:55:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:54:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:21:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:20:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:47:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:46:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:13:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:12:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:39:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:38:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:05:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:04:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:31:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:30:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:57:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:56:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:23:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:22:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:49:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:48:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:15:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:14:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:41:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:40:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:08:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:07:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:34:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:33:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:50:00 PST 2014"),"taskName":"PS A","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:02:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:29:00 PST 2014"),"taskName":"PS B","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:28:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:55:00 PST 2014"),"taskName":"PS B","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:54:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:21:00 PST 2014"),"taskName":"PS B","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:20:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:47:00 PST 2014"),"taskName":"PS B","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:47:03 PST 2014"),"endDate":new Date("Sun Feb 19 12:12:00 PST 2014"),"taskName":"PS B","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:28:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:55:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:54:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:21:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:20:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:47:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:46:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:13:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:12:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:39:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:38:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:05:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:04:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:31:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:30:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:57:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:56:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:23:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:22:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:49:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:48:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:15:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:14:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:42:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:40:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:08:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:07:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:34:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:33:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:50:00 PST 2014"),"taskName":"PS C","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:01:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:28:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:27:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:54:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:54:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:21:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:20:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:47:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:46:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:13:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:12:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:39:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:39:00 PST 2014"),"endDate":new Date("Sun Feb 19 12:41:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:41:00 PST 2014"),"endDate":new Date("Sun Feb 19 12:41:04 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:40:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:07:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:06:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:33:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:32:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:59:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:58:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:25:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:24:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:51:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:50:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:17:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:17:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:44:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:42:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:10:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:09:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:36:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:33:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:50:00 PST 2014"),"taskName":"PS F","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:03:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:30:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:29:56 PST 2014"),"endDate":new Date("Sun Feb 19 10:56:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 10:55:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:22:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:21:56 PST 2014"),"endDate":new Date("Sun Feb 19 11:48:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 11:47:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:14:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:13:56 PST 2014"),"endDate":new Date("Sun Feb 19 12:40:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 12:39:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:06:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:05:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:32:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:31:56 PST 2014"),"endDate":new Date("Sun Feb 19 13:58:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 13:57:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:24:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:23:56 PST 2014"),"endDate":new Date("Sun Feb 19 14:50:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 14:50:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:17:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:16:56 PST 2014"),"endDate":new Date("Sun Feb 19 15:43:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 15:41:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:09:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:08:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:35:00 PST 2014"),"taskName":"PS G","status":"A"},
+{"startDate":new Date("Sun Feb 19 16:34:56 PST 2014"),"endDate":new Date("Sun Feb 19 16:51:00 PST 2014"),"taskName":"PS G","status":"A"},
+];
+
+var taskStatus = {
+    "A" : "bar-a"
+};
+
+var taskNames = [ "PS A", "PS B", "PS C", "PS D", "PS E", "PS F", "PS G" ];
+
+tasks.sort(function(a, b) {
+    return a.endDate - b.endDate;
+});
+var maxDate = tasks[tasks.length - 1].endDate;
+tasks.sort(function(a, b) {
+    return a.startDate - b.startDate;
+});
+var minDate = tasks[0].startDate;
+
+var format = "%H:%M";
+
+var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+gantt(tasks);
